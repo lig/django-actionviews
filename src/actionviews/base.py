eab569dcb0e9cc1a8b43aa7890690b8a1c27435c
Serge Matveenko <s@matveenko.ru>
2013-12-02 19:46:42 +0400

    Allow custom urls.

--- src/actionviews/base.py
+++ src/actionviews/base.py
@@ -30,48 +30,50 @@ class ActionViewMeta(type):
 
         type_new.actions = actions
 
-        # construct urls
-        urls = []
+        # construct urls if there is no custom urls defined
+        if 'urls' not in attrs:
+            urls = []
 
-        for action_name, action_method in type_new.actions.items():
-            regex_chunks = []
-            default_values = {}
-            parameters = inspect.signature(action_method).parameters.values()
+            for action_name, action_method in type_new.actions.items():
+                regex_chunks = []
+                default_values = {}
+                parameters = inspect.signature(
+                    action_method).parameters.values()
 
-            for parameter in parameters:
+                for parameter in parameters:
 
-                if parameter.name == 'self':
-                    group_name = (parameter.annotation is inspect._empty
-                        and action_name
-                        or parameter.annotation)
-                    sep = group_name and '/'
-                    regex_chunks.append(r'{}{}'.format(group_name, sep))
-                    continue
+                    if parameter.name == 'self':
+                        group_name = (parameter.annotation is inspect._empty
+                            and action_name
+                            or parameter.annotation)
+                        sep = group_name and '/'
+                        regex_chunks.append(r'{}{}'.format(group_name, sep))
+                        continue
 
-                group_name = parameter.name
+                    group_name = parameter.name
 
-                if parameter.annotation is inspect._empty:
-                    group_regex = type_new.default_group_regex
-                else:
-                    group_regex = parameter.annotation
+                    if parameter.annotation is inspect._empty:
+                        group_regex = type_new.default_group_regex
+                    else:
+                        group_regex = parameter.annotation
 
-                if parameter.default is inspect._empty:
-                    group_format = type_new.group_format
-                else:
-                    default_values[parameter.name] = parameter.default
-                    group_format = r'({})?'.format(type_new.group_format)
+                    if parameter.default is inspect._empty:
+                        group_format = type_new.group_format
+                    else:
+                        default_values[parameter.name] = parameter.default
+                        group_format = r'({})?'.format(type_new.group_format)
 
-                regex_chunks.append(group_format.format(
-                    group_name=group_name, group_regex=group_regex))
+                    regex_chunks.append(group_format.format(
+                        group_name=group_name, group_regex=group_regex))
 
-            url_regex = r'^{}$'.format(''.join(regex_chunks))
-            urls.append(url(
-                regex=url_regex,
-                view=None,
-                kwargs=default_values,
-                name=action_name))
+                url_regex = r'^{}$'.format(''.join(regex_chunks))
+                urls.append(url(
+                    regex=url_regex,
+                    view=None,
+                    kwargs=default_values,
+                    name=action_name))
 
-        type_new.urls = urls
+            type_new.urls = urls
 
         return type_new
 
--- tests/test_base.py
+++ tests/test_base.py
@@ -2,7 +2,7 @@ import pytest
 from operator import attrgetter
 
 from django.core.urlresolvers import RegexURLPattern
-from django.conf.urls import patterns
+from django.conf.urls import patterns, url
 
 from actionviews.base import ActionView
 
@@ -102,3 +102,32 @@ def test_urls_default(TestView):
     assert urls_data['detail'].default_args == {}
     assert urls_data['article'].default_args == {}
     assert urls_data['default'].default_args == {}
+
+
+def test_urls_custom(TestView):
+
+    class CustomView(TestView):
+        urls = patterns('',
+            # @todo: check for correct view
+            url(r'^(page/(?P<skip>\d+)/)?$',
+                'index',
+                kwargs={'skip': 0},
+                name='list'),
+            url(r'^detail/(?P<id>\d+)/$', 'detail', name='detail'),
+        )
+
+    urls = CustomView.urls
+
+    assert all(map(lambda x: isinstance(x, RegexURLPattern), urls))
+
+    urls_data = {url.name: url for url in urls}
+
+    assert urls_data['list']._regex == r'^(page/(?P<skip>\d+)/)?$'
+    assert urls_data['detail']._regex == r'^detail/(?P<id>\d+)/$'
+
+    assert urls_data['list'].default_args == {'skip': 0}
+    assert urls_data['detail'].default_args == {}
+
+    assert 'index' not in urls_data
+    assert 'article' not in urls_data
+    assert 'default' not in urls_data
